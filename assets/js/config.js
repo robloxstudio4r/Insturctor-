6ce789bc0b9a6125e6be56d43cc421a4d5bdd5a7
robloxstudio4r <textaway2@outlook.com>
2026-09-18 17:16:55 -0500

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://bcftqqgkixtluztnteyp.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_67h9fX7eDUGOZS6ZhrGq1Q_QthipA6b';
+export const APP_NAME = 'Instructor+';
